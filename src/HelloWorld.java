52c5ebd1eb03404c68f10fab5ad9a8b76b61db4f
yabingrigory8893 <yabingrigory3@gmail.com>
2026-04-09 22:41:55 -0700

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,6 @@
 public class HelloWorld {
-    // TODO: Write your program here to print the required output
+    public static void main(String[] args){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
 }
